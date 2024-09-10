8d5979a8cfb63c7549da17622bc357c4eada83cc
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2024-09-10 18:47:18 +0500

    JSON PlaceHolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,134 +1,140 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { data } from "./data";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [todos, setTodos] = useState(data);
-  const [newTodo, setNewTodo] = useState("");
-  const [editTodoId, setEditTodoId] = useState("");
-  const [editTodoText, setEditTodoText] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [newTodo, setNewTodo] = useState("");
+	const [editTodoId, setEditTodoId] = useState("");
+	const [editTodoText, setEditTodoText] = useState("");
 
-  const handleAddTodo = (event) => {
-    event.preventDefault();
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/todos")
+			.then((response) => response.json())
+			.then((json) => setTodos(json));
+	}, []);
 
-    if (newTodo.trim()) {
-      setTodos((prevTodos) => [
-        ...prevTodos,
-        { title: newTodo, id: Date.now() },
-      ]);
-      setNewTodo("");
-      toast.success("Ma'lumot muvoffaqiyatli qo'shildi!");
-    } else {
-      toast.error("Maydon bo'sh bo'lishi mumkin emas!");
-    }
-  };
+	const handleAddTodo = (evt) => {
+		evt.preventDefault();
+		if (newTodo) {
+			const newTodoItem = { title: newTodo, id: Date.now(), completed: false };
+			setTodos((prevTodos) => [...prevTodos, newTodoItem]);
+			setNewTodo("");
+			toast.success("Ma'lumot muvofaqqiyatli qo'shildi!");
+		} else {
+			toast.error("Joy bo'lishi mumkin emas!");
+		}
+	};
 
-  const handleDeleteTodo = (id) => {
-    const filteredTodos = todos.filter(todo => todo.id !== id);
-    setTodos(filteredTodos);
-    toast.error("Ma'lumot o'chirildi!");
-  };
+	const handleDeleteTodo = (id) => {
+		fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+			method: "DELETE",
+		}).then((response) => {
+			const filteredTodos = todos.filter((todo) => todo.id !== id);
+			setTodos(filteredTodos);
+			toast.error("Ma'lumot o'chirildi!");
+		});
+	};
 
-  const handleEditTodo = (id) => {
-    const todoToEdit = todos.find(todo => todo.id === id);
-    setEditTodoId(id);
-    setEditTodoText(todoToEdit.title);
-  };
+	const handleEditTodo = (id) => {
+		const todoToEdit = todos.find((todo) => todo.id === id);
+		setEditTodoId(id);
+		setEditTodoText(todoToEdit.title);
+	};
 
-  const handleSaveEdit = (id) => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === id ? { ...todo, title: editTodoText } : todo
-    );
-    setTodos(updatedTodos);
-    setEditTodoId("");
-    setEditTodoText("");
-    toast.success("Ma'lumot yangilandi!");
-  };
+	const handleSaveEdit = (id) => {
+		const updatedTodos = todos.map((todo) =>
+			todo.id === id ? { ...todo, title: editTodoText } : todo
+		);
+		setTodos(updatedTodos);
+		setEditTodoId();
+		setEditTodoText("");
+		toast.success("Ma'lumot yangilandi!");
+	};
 
-  const handleCancelEdit = () => {
-    setEditTodoId("");
-    setEditTodoText("");
-  };
+	const handleCancelEdit = () => {
+		setEditTodoId();
+		setEditTodoText("");
+	};
 
-  return (
-    <div className="todos">
-      <div className="todos__block">
-        <ToastContainer />
-        <h1 className="todo__title">Todo List</h1>
-        <form className="todoForm" onSubmit={handleAddTodo}>
-          <input
-            type="text"
-            className="todo_input"
-            placeholder="Enter the tasks"
-            value={newTodo}
-            onChange={(evt) => setNewTodo(evt.target.value)}
-          />
-          <button type="submit" className="todo_btn">
-            Add Task
-          </button>
-        </form>
-        <ul className="todos__data">
-          {todos.map((todo) => (
-            <li className="todos_item" key={todo.id} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-    <input
-        type="checkbox"
-        checked={todo.completed}
-        onChange={() => handleCheckboxChange(todo.id)}
-    />
+	const handleCheckboxChange = (id) => {
+		const updatedTodos = todos.map((todo) =>
+			todo.id === id ? { ...todo, completed: !todo.completed } : todo
+		);
+		setTodos(updatedTodos);
+	};
 
-              {editTodoId === todo.id ? (
-                <>
-                  <input className="edit_modal"
-                    type="text"
-                    value={editTodoText}
-                    onChange={(e) => setEditTodoText(e.target.value)}
-                  />
-                  <button
-                    onClick={() => handleSaveEdit(todo.id)}
-                    className="edit_btn_save"
-                  >
-                    Save
-                  </button>
-                  <button
-                    onClick={handleCancelEdit}
-                    className="edit_btn_cancel "
-                  >
-                    Cancel
-                  </button>
-                </>
-              ) : (
-                <>
-                  <button
-                    onClick={() => handleEditTodo(todo.id)}
-                    className="todos__btn edit"
-                  >
-                    <img
-                      className="btn"
-                      src="https://img.icons8.com/?size=100&id=MsQIVqWh2kj2&format=png&color=000000"
-                      alt="Edit"
-                    />
-                  </button>
-                  <button
-                    onClick={() => handleDeleteTodo(todo.id)}
-                    className="todos__btn delete"
-                  >
-                    <img
-                      className="btn"
-            src="https://img.icons8.com/?size=100&id=Ak1nWJFsk3c7&format=png&color=000000"
-                      alt="Delete"
-                    />
-                  </button>
-                  {todo.title}
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="todos">
+			<div className="todos__block">
+				<ToastContainer />
+				<h1 className="todo__title">Todo List</h1>
+				<form className="todoForm" onSubmit={handleAddTodo}>
+					<input
+						type="text"
+						className="todo_input"
+						placeholder="Enter the tasks"
+						value={newTodo}
+						onChange={(evt) => setNewTodo(evt.target.value)}
+					/>
+					<button type="submit" className="todo_btn">
+						Add Task
+					</button>
+				</form>
+				<ul className="todos__data">
+					{todos.map((todo) => (
+						<li
+							className="todos_item"
+							key={todo.id}
+							style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+						>
+							<input
+								className="checkbox_input"
+								type="checkbox"
+								checked={todo.completed}
+								onChange={() => handleCheckboxChange(todo.id)}
+							/>
+							{editTodoId === todo.id ? (
+								<>
+									<input
+										className="edit_modal"
+										type="text"
+										value={editTodoText}
+										onChange={(evt) => setEditTodoText(evt.target.value)}
+									/>
+									<button onClick={() => handleSaveEdit(todo.id)} className="edit_btn_save">
+										Save
+									</button>
+									<button onClick={handleCancelEdit} className="edit_btn_cancel">
+										Cancel
+									</button>
+								</>
+							) : (
+								<>
+									<button onClick={() => handleEditTodo(todo.id)} className="todos__btn edit">
+										<img
+											className="btn"
+											src="https://img.icons8.com/?size=100&id=MsQIVqWh2kj2&format=png&color=000000"
+											alt="Edit"
+										/>
+									</button>
+									<button onClick={() => handleDeleteTodo(todo.id)} className="todos__btn delete">
+										<img
+											className="btn"
+											src="https://img.icons8.com/?size=100&id=Ak1nWJFsk3c7&format=png&color=000000"
+											alt="Delete"
+										/>
+									</button>
+									{todo.title}
+								</>
+							)}
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
